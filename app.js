c442a2b4828a92d8fd38c8d9c2934668ad680360
lelouch248 <lelouch248@users.noreply.github.com>
2023-01-26 14:22:39 +0530

    added mongo db atlas for the db storage

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
 // Load the full build.
-var lo = require('lodash');
+var lo = require("lodash");
+
+mongoose.connect(
+  "mongodb+srv://Karthik:karthik@cluster0.fohsgzm.mongodb.net/blogDB",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -11,8 +22,6 @@ const aboutContent =
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-var blogPosts = [];
-
 const app = express();
 
 app.set("view engine", "ejs");
@@ -20,10 +29,21 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const postsSchema = {
+  title: String,
+  post: String,
+};
+
+const Post = mongoose.model("Post", postsSchema);
+
 app.get("/", function (req, res) {
-  res.render("home", {
-    homeStartContent: homeStartingContent,
-    posts: blogPosts,
+  let posts = [];
+  Post.find({}, function (err, foundPosts) {
+    res.render("home", {
+      homeStartContent: homeStartingContent,
+      posts: foundPosts,
+    });
+    console.log(foundPosts);
   });
 });
 
@@ -39,30 +59,28 @@ app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.get("/posts/:postName", function (req, res) {
-  let requestedTitle = req.params.postName;
-  blogPosts.forEach(function (post) {
-    
-    let storedTitle = lo.kebabCase(lo.lowerCase(post.title));
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        body: post.body,
-      });
-    }else{
-      console.log("No match found!");
-    }
-  });
+app.get("/posts/:postID", function (req, res) {
+const requestedTitleID = req.params.postID;  
+  Post.findOne({_id:requestedTitleID},function(err,foundPost){
+    res.render("post", {
+      title: foundPost.title,
+      post: foundPost.post,
+    });
+});
 });
 
 
 app.post("/compose", function (req, res) {
-  let post = {
+  const post = new Post({
     title: req.body.postTitle,
-    body: req.body.postBody,
-  };
-  blogPosts.push(post);
-  res.redirect("/");
+    post: req.body.postBody,
+  });
+  console.log(post);
+  post.save(function (err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 });
 
 app.listen(3000, function () {
